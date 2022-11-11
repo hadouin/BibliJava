775c03162846069255715c9ad97b66001f5eccba
Hadouin Leroy <hadouinl@gmail.com>
2022-11-11 15:55:06 +0100

    bibli constructor

--- BibliJava/src/Bibli.java
+++ BibliJava/src/Bibli.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bibli {
@@ -5,4 +6,11 @@ public class Bibli {
     public String name;
     List<Ressource> resources;
     List<Emprunt> emprunts;
+
+    public Bibli(String name, int codePostal) {
+        this.codePostal = codePostal;
+        this.name = name;
+        resources = new ArrayList<Ressource>();
+        emprunts = new ArrayList<Emprunt>();
+    }
 }
